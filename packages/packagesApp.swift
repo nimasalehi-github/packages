ea67ac047314b45eaf808707f62a0aff6763ddca
n salehi <nimasalehi.auth@icloud.com>
2025-11-06 12:51:08 +0330

    Initial Commit

---
+++ packages/packagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  packagesApp.swift
+//  packages
+//
+//  Created by Nima Salehi on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct packagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
